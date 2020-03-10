ef9fce336aad1da6dc4d462166905b01af97696d
yu9330 <neko.love.1027@gmail.com>
2020-03-11 07:58:54 +0900

    27th commit

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,12 +5,12 @@ class FavoritesController < ApplicationController
     # buildはnewと同じ意味で、アソシエーションしながらインスタンスをnewする時に形式的に使われる。
     favorite = current_user.favorites.build(book_id: params[:book_id])
     favorite.save
-    redirect_to book_path
+    redirect_to users_show_path(id: current_user.id)
   end
 
   def destroy
     favorite = Favorite.find_by(book_id: params[:book_id], user_id: current_user.id)
     favorite.destroy
-    redirect_to book_path
+    redirect_to users_show_path(id: current_user.id)
   end
 end
